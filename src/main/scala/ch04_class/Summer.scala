3644348dae1aa931746cde4a12ae644c3f6680ca
khs <hyeoksuk@gmail.com>
2024-01-03 00:09:32 +0900

    240101 스터디 - 4장 복습 완료

--- src/main/scala/ch04_class/Summer.scala
+++ src/main/scala/ch04_class/Summer.scala
@@ -1,7 +1,8 @@
 package ch04_class
 
 object Summer {
-  def main(args: Array[String]): Unit = {
+  // 아래와 정확히 동일한 시그니처의 메서드를 가지면 어떤 독립 객체든 애플리케이션의 시작점 역할을 할 수 있다.
+  def main(args: Array[String]) = {
     for(arg <- args)
       println(s"${arg} : ${ChecksumAccumulator.calculate(arg)}")
   }
--- src/main/scala/ch04_class/Summer2.scala
+++ src/main/scala/ch04_class/Summer2.scala
@@ -4,7 +4,7 @@ package ch04_class
 object Summer2 extends App {
   for(season <- List("fall", "winter", "spring"))
     println(s"${season} : ${ChecksumAccumulator.calculate(season)}")
-  // main 처럼 암시적으로 선언된 args를 통해 명령행 인자에 접근할 수 있음
+  // App 트레이트에 있는 args를 통해 명령행 인자에 접근할 수 있음
   for(arg <- args)
     println(arg)
 }
